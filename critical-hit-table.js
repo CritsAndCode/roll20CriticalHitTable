c9149982e33e27e292fba299f5806cc35e8d024a
Catlin Buckley <catlinbuckley@users.noreply.github.com>
2017-03-22 20:11:43 -0400

    added more damage types
    added force, lightning, necrotic, poison, psychic, radiant, and thunder crit tables

--- critical-hit-table.js
+++ critical-hit-table.js
@@ -84,7 +84,105 @@ var Critical = (function()
 		{low: 14,  high: 16,  result: "Burnt to a crisp!", damage: "Roll twice as many damage dice as normal and the creature is charred. If the creature has resistance to fire, it loses that resistance. If the creature does not have resistance to fire, it gains vulnerability to fire. Both of these effects can be ended by lesser restoration or it completes the recuperate downtime activity."},
 		{low: 17,  high: 18,  result: "Hellfire!", damage: "Deal the maximum amount of damage from your normal damage dice then roll your damage dice and add that result then roll on the minor injury chart. Additionally, the creature is on fire. While the creature is on fire it takes 2d6 fire damage at the start of each of its turns. The creature can end this condition by dropping prone and using 5 feet of movement to roll on the ground."},
 		{low: 19,  high: 19,  result: "Combustion!", damage: "Deal the twice maximum result of your damage dice.", majorInjury: true},
-		{low: 20,  high: 20,  result: "Inferno!", damage: "Deal twice the maximum result of your damage dice and roll on the major injury chart. Additionally, the creature is on fire. While the creature is on fire it takes 2d8 fire damage at the start of each of its turns. The creature can end this condition by dropping prone and using 5 feet of movement to roll on the ground."}
+		{low: 20,  high: 20,  result: "Inferno!", damage: "Deal twice the maximum result of your damage dice. Additionally, the creature is on fire. While the creature is on fire it takes 2d8 fire damage at the start of each of its turns. The creature can end this condition by dropping prone and using 5 feet of movement to roll on the ground.", majorInjury: true}
+	];
+	
+	const forceCritTable =
+	[
+		{low: 1,  high: 1,  result: "You call that a crit?", damage: "Roll damage as normal."},
+		{low: 2,  high: 3,  result: "Spellstruck!", damage: "Roll damage as normal and the creature has disadvantage on saving throws against spells until the end of its next turn."},
+		{low: 4,  high: 6,  result: "Good hit!", damage: "Do not roll your damage dice, instead deal the maximum result possible with those dice."},
+		{low: 7,  high: 8,  result: "Eldritch incandescence!", damage: "Roll your damage as normal and spell attack rolls against the creature have advantage until the end of its next turn."},
+		{low: 9,  high: 11,  result: "Great hit!", damage: "Roll your damage dice twice and add them together."},
+		{low: 12,  high: 13,  result: "Bewitching blow!", damage: "Roll twice as many damage dice as normal and the creature is spellbound until the end of its next turn. While spellbound it makes saving throws against spells with disadvantage and spell attack rolls against it have advantage."},
+		{low: 14,  high: 16,  result: "Mystic magnet!", damage: "Roll twice as many damage dice as normal and the creature is spellbound for the next minute. While spellbound it makes saving throws against spells with disadvantage and spell attack rolls against it have advantage. At the end of each of the creature’s turns it can make an Intelligence saving throw (DC 14) to end this effect."},
+		{low: 17,  high: 18,  result: "Ensorcelled!", damage: "Deal the maximum amount of damage from your normal damage dice then roll your damage dice and add that result. Additionally, the creature is spellbound for the next minute. While spellbound it makes saving throws against spells with disadvantage and spell attack rolls against it have advantage. At the end of each of the creature’s turns it can make an Intelligence saving throw (DC 16) to end this effect.", minorInjury: true},
+		{low: 19,  high: 19,  result: "Arcane injury!", damage: "Deal the twice maximum result of your damage dice.", majorInjury: true},
+		{low: 20,  high: 20,  result: "Magically mauled!", damage: "Deal twice the maximum result of your damage dice. Additionally, the creature is spellbound for the next minute. While spellbound it makes saving throws against spells with disadvantage and spell attack rolls against it have advantage. At the end of each of the creature’s turns it can make an Intelligence saving throw (DC 18) to end this effect.", majorInjury: true}
+	];
+	
+	const lightningCritTable =
+	[
+		{low: 1,  high: 1,  result: "You call that a crit?", damage: "Roll damage as normal."},
+		{low: 2,  high: 3,  result: "Shocking!", damage: "Roll damage as normal and the creature cannot use reactions until the end of its next turn."},
+		{low: 4,  high: 6,  result: "Good hit!", damage: "Do not roll your damage dice, instead deal the maximum result possible with those dice."},
+		{low: 7,  high: 8,  result: "Sparks fly!", damage: "Roll your damage as normal and you may choose one other creature within 15 ft. of the victim. That creature must succeed on a Dexterity saving throw (DC 14) or take half as much damage."},
+		{low: 9,  high: 11,  result: "Great hit!", damage: "Roll your damage dice twice and add them together."},
+		{low: 12,  high: 13,  result: "Electric arc!", damage: "Roll twice as many damage dice as normal and you may choose one other creature within 15 ft. of the victim. That creature must succeed on a Dexterity saving throw (DC 18) or take half as much damage."},
+		{low: 14,  high: 16,  result: "Fulminate!", damage: "Roll twice as many damage dice as normal. If the creature is wearing metal armor roll it receives a Major Injury. Otherwise, it receives a Minor Injury.", minorInjury: true, majorInjury: true},
+		{low: 17,  high: 18,  result: "Lit up!", damage: " Deal the maximum amount of damage from your normal damage dice then roll your damage dice and add that result. The creature and each creature you choose within 15 ft. of it cannot take reactions until the end of their next turn.", minorInjury: true},
+		{low: 19,  high: 19,  result: "Electrocuted!", damage: "Deal the twice maximum result of your damage dice.", majorInjury: true},
+		{low: 20,  high: 20,  result: "Lightning rod!", damage: "Deal twice the maximum result of your damage dice and you may choose one other creature within 15 ft. of the victim. That creature must succeed on a Dexterity saving throw (DC 20) or take half as much damage.", majorInjury: true}
+	];
+	
+	const necroticCritTable =
+	[
+		{low: 1,  high: 1,  result: "You call that a crit?", damage: "Roll damage as normal."},
+		{low: 2,  high: 3,  result: "Spoil!", damage: "Roll damage as normal and the creature cannot regain hit points until the end of its next turn."},
+		{low: 4,  high: 6,  result: "Good hit!", damage: "Do not roll your damage dice, instead deal the maximum result possible with those dice."},
+		{low: 7,  high: 8,  result: "Fester!", damage: "Roll your damage as normal and the creature’s maximum hit points are reduced by the same amount."},
+		{low: 9,  high: 11,  result: "Great hit!", damage: "Roll your damage dice twice and add them together."},
+		{low: 12,  high: 13,  result: "Decay!", damage: "Roll twice as many damage dice as normal and the creature’s maximum hit points are reduced by the same amount."},
+		{low: 14,  high: 16,  result: "Rot!", damage: "Roll twice as many damage dice as normal and the creature cannot regain hit points for the next minute. It may make a saving throw (DC 16) at the end of each of its turns to end this effect."},
+		{low: 17,  high: 18,  result: "Blight!", damage: "Deal the maximum amount of damage from your normal damage dice then roll your damage dice and add that result. The creature’s maximum hit points are reduced by the same amount.", minorInjury: true},
+		{low: 19,  high: 19,  result: "Atrophy!", damage: "Deal the twice maximum result of your damage dice.", majorInjury: true},
+		{low: 20,  high: 20,  result: "Putrefy!", damage: "Deal twice the maximum result of your damage dice, the creature’s maximum hit points are reduced by the same amount, and the creature cannot regain hit points until the end of its next turn.", majorInjury: true}
+	];
+	
+	const poisonCritTable =
+	[
+		{low: 1,  high: 1,  result: "You call that a crit?", damage: "Roll damage as normal."},
+		{low: 2,  high: 3,  result: "Nauseous!", damage: "Roll damage as normal and the creature has disadvantage on its next ability check, attack roll, or saving throw."},
+		{low: 4,  high: 6,  result: "Good hit!", damage: "Do not roll your damage dice, instead deal the maximum result possible with those dice."},
+		{low: 7,  high: 8,  result: "Sickened!", damage: "Roll your damage as normal and the creature has disadvantage on all ability checks, attack rolls, and saving throws until the end of its next turn."},
+		{low: 9,  high: 11,  result: "Great hit!", damage: "Roll your damage dice twice and add them together."},
+		{low: 12,  high: 13,  result: "Poisoned!", damage: "Roll twice as many damage dice as normal and the creature is poisoned for the next minute. The creature may attempt a saving throw at the end of each of its turns (DC 12) to end this effect."},
+		{low: 14,  high: 16,  result: "Contaminated!", damage: "Roll twice as many damage dice as normal and the creature is poisoned for the next minute. The creature may attempt a saving throw at the end of each of its turns (DC 16) to end this effect."},
+		{low: 17,  high: 18,  result: "Toxic shock!", damage: "Deal the maximum amount of damage from your normal damage dice then roll your damage dice and add that result. The creature is poisoned for the next minute. The creature may attempt a saving throw at the end of each of its turns (DC 12) to end this effect.", minorInjury: true},
+		{low: 19,  high: 19,  result: "System failure!", damage: "Deal the twice maximum result of your damage dice.", majorInjury: true},
+		{low: 20,  high: 20,  result: "Biological breakdown!", damage: "Deal twice the maximum result of your damage dice and the creature is poisoned for the next minute. The creature may attempt a saving throw at the end of each of its turns (DC 16) to end this effect.", majorInjury: true}
+	];
+	
+	const psychicCritTable =
+	[
+		{low: 1,  high: 1,  result: "You call that a crit?", damage: "Roll damage as normal."},
+		{low: 2,  high: 3,  result: "Disoriented!", damage: " Roll your damage dice as normal and you control the creature’s movement on its next turn."},
+		{low: 4,  high: 6,  result: "Good hit!", damage: "Do not roll your damage dice, instead deal the maximum result possible with those dice."},
+		{low: 7,  high: 8,  result: "Confused!", damage: "Roll your damage dice as normal and the creature cannot differentiate friend from foe until the end of its next turn."},
+		{low: 9,  high: 11,  result: "Great hit!", damage: "Roll your damage dice twice and add them together."},
+		{low: 12,  high: 13,  result: "Dominated!", damage: "Roll twice as many damage dice as normal and you control the creature’s action on its next turn."},
+		{low: 14,  high: 16,  result: "Psychological fracture!", damage: "Roll twice as many damage dice as normal and the creature gains a level of madness."},
+		{low: 17,  high: 18,  result: "Psychological break!", damage: "Deal the maximum amount of damage from your normal damage dice then roll your damage dice and add that result. Then the creature gains a level of madness. "},
+		{low: 19,  high: 19,  result: "Madness!", damage: "Deal the twice maximum result of your damage dice and gain a level of madness."},
+		{low: 20,  high: 20,  result: "Mind melt!", damage: "Deal twice the maximum result of your damage dice and gain two levels of madness."}
+	];
+	
+	const radiantCritTable =
+	[
+		{low: 1,  high: 1,  result: "You call that a crit?", damage: "Roll damage as normal."},
+		{low: 2,  high: 3,  result: "Dazzled!", damage: "Roll damage as normal and the creature cannot willingly move closer to you until the end of its next turn."},
+		{low: 4,  high: 6,  result: "Good hit!", damage: "Do not roll your damage dice, instead deal the maximum result possible with those dice."},
+		{low: 7,  high: 8,  result: "Disheartening blast!", damage: "Roll your damage as normal and the creature is frightened until the end of its next turn."},
+		{low: 9,  high: 11,  result: "Great hit!", damage: "Roll your damage dice twice and add them together."},
+		{low: 12,  high: 13,  result: "Awed!", damage: "Roll twice as many damage dice as normal and the creature is frightened until the end of its next turn."},
+		{low: 14,  high: 16,  result: "Holy terror!", damage: "Roll twice as many damage dice as normal. Additionally, the creature is frightened for the next minute. It can make a Wisdom saving throw (DC 16) at the end of each of its turns to end this effect.", minorInjury: true},
+		{low: 17,  high: 18,  result: "Righteous mark!", damage: " Deal the maximum amount of damage for your normal damage dice then roll your damage dice and add that result, then roll on the minor injury chart. Additionally, the creature glows for the next minute. While glowing it produces bright light up 10 feet and dim light up to 30 feet and all successful attacks against the creature deal an additional 1d4 radiant damage."},
+		{low: 19,  high: 19,  result: "Wrath of the gods!", damage: "Deal the twice maximum result of your damage dice.", majorInjury: true},
+		{low: 20,  high: 20,  result: "Smote!", damage: "Deal twice the maximum result of your damage dice and the creature glows for the next minute. While glowing it produces bright light up 10 feet and dim light up to 30 feet and all successful attacks against the creature deal an additional 1d6 radiant damage.", majorInjury: true}
+	];
+	
+	const thunderCritTable =
+	[
+		{low: 1,  high: 1,  result: "You call that a crit?", damage: "Roll damage as normal."},
+		{low: 2,  high: 3,  result: "Boom!", damage: "Roll damage as normal and the creature is deafened until the end of its next turn."},
+		{low: 4,  high: 6,  result: "Good hit!", damage: "Do not roll your damage dice, instead deal the maximum result possible with those dice."},
+		{low: 7,  high: 8,  result: "Ka-boom!", damage: "Roll your damage as normal and the creature is deafened for one minute."},
+		{low: 9,  high: 11,  result: "Great hit!", damage: "Roll your damage dice twice and add them together."},
+		{low: 12,  high: 13,  result: "Thunder clap!", damage: "Roll twice as many damage dice as normal and the creature is stunned until the start of its next turn and deafened for one minute."},
+		{low: 14,  high: 16,  result: "Burst ear drums!", damage: "Roll twice as many damage dice as normal and the creature is deafened permanently.", minorInjury: true},
+		{low: 17,  high: 18,  result: "Concussive blast!", damage: "Deal the maximum amount of damage from your normal damage dice then roll your damage dice and add that result. The creature stunned until the end of its next turn and deafened permanently.", minorInjury: true},
+		{low: 19,  high: 19,  result: "Wall of sound!", damage: "Deal the twice maximum result of your damage dice.", majorInjury: true},
+		{low: 20,  high: 20,  result: "Sonic salvo!", damage: "Deal twice the maximum result of your damage dice, the creature is deafened permanently, and stunned until the end of its next round.", majorInjury: true}
 	];
 	
 	const minorInjuryTable = 
@@ -164,6 +262,27 @@ var Critical = (function()
 			            case 'fire':
 				            crit = _determineCritical(roll, fireCritTable);
 				            break;
+				        case 'force':
+				            crit = _determineCritical(roll, forceCritTable);
+				            break;   
+				        case 'lightning':
+				            crit = _determineCritical(roll, lightningCritTable);
+				            break;
+				        case 'necrotic':
+				            crit = _determineCritical(roll, necroticCritTable);
+				            break;   
+			            case 'poison':
+				            crit = _determineCritical(roll, poisonCritTable);
+				            break;
+			            case 'psychic':
+				            crit = _determineCritical(roll, psychicCritTable);
+				            break;
+			            case 'radiant':
+				            crit = _determineCritical(roll, radiantCritTable);
+				            break;
+			            case 'thunder':
+				            crit = _determineCritical(roll, thunderCritTable);
+				            break;
 				    }
 				    
 				    if (crit) {
@@ -175,7 +294,8 @@ var Critical = (function()
 				}
 				else
 				{
-					//dropdown of damage types
+						sendChat("Critical Hits", "Try !critical followed by bludgeoning, piercing, slashing, acid, cold, fire, force, lightning, necrotic, poison, psychic, radiant, or thunder.");
+
 				}
 			}
 		}
